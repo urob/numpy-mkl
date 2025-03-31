b78c2cddb40a3ba09d3aa8d809a957da15e5044c
urob <978080+urob@users.noreply.github.com>
2025-03-31 17:10:11 -0400

    Fix numpy tests failing on Windows

--- tools/numpy_tests.py
+++ tools/numpy_tests.py
@@ -1,8 +1,15 @@
+import os
 import sys
+from pathlib import Path
 
 import numpy as np
 
 np.show_config()
+
+if os.environ['RUNNER_OS'] == 'Windows':
+    # GH 20391
+    libs = Path(sys.prefix) / 'libs'
+    libs.mkdir(parents=True, exist_ok=True)
 
 passed = np.test(
     label='full', extra_argv=['-n=auto', '--timeout=1800', '--durations=10']

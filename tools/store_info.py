f99404c47268af67f68cd54ca1e07cd36d4af551
urob <978080+urob@users.noreply.github.com>
2025-03-31 15:08:02 -0400

    Fix import error

--- tools/store_info.py
+++ tools/store_info.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
 import argparse
+import contextlib
 import json
-import sys
 from pathlib import Path
 
-import semver
+with contextlib.suppress(ImportError):
+    import semver
 
 
 class Build:
-    def __init__(self, data):
-        if isinstance(data, dict):
-            self.data = data
+    def __init__(self, info):
+        if isinstance(info, dict):
+            self.info = info
         else:
-            self.data = json.loads(data.read_text(encoding='UTF-8'))
+            self.info = json.loads(info.read_text(encoding='UTF-8'))
 
-        self.name = self.data['name']
-        self.version = self.data['version']
-        self.python = self.data['python']
-        self.os = self.data['os'].lower()
-        self.mkl = self.data['mkl']
+        self.name = self.info['name']
+        self.version = self.info['version']
+        self.python = self.info['python']
+        self.os = self.info['os'].lower()
+        self.mkl = self.info['mkl']
 
         self.key = '-'.join([self.name, self.version, self.python, self.os])
 
@@ -38,15 +39,7 @@ def fetch_store(path):
     return json.loads(path.read_text(encoding='UTF-8')) if path.exists() else {}
 
 
-def check_builds(storepath, builds):
-    store = fetch_store(storepath)
-    for b in builds:
-        if not Build(b).exclude(store):
-            return sys.exit(0)
-    sys.exit(1)
-
-
-def merge_builds(storepath, builds):
+def main(storepath, builds):
     store = fetch_store(storepath)
     for b in builds:
         Build(b).merge_with(store)
@@ -59,13 +52,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Store build meta data')
     parser.add_argument('builds', nargs='+', help='input file(s)')
     parser.add_argument('-s', '--store', required=True, type=str, help='output file')
-    parser.add_argument('--check', action='store_true', help='check if build exists')
 
     args = parser.parse_args()
     store = Path(args.store)
     builds = [Path(f) for f in args.builds]
 
-    if args.check:
-        check_builds(store, builds)
-    else:
-        merge_builds(store, builds)
+    main(store, builds)

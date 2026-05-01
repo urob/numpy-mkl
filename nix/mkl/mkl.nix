757dc856771df3fe859a71e252a82e4422cb9bf1
urob <978080+urob@users.noreply.github.com>
2026-05-01 15:28:05 -0400

    Fix incompatible mkl version in nix workflow

--- nix/mkl/mkl.nix
+++ nix/mkl/mkl.nix
@@ -7,24 +7,24 @@
 }:
 buildPythonPackage rec {
   pname = "mkl";
-  version = "2026.0.0";
+  version = "2025.3.0";
   format = "wheel";
 
   src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/91/97/06ad1072db8a3c4d1e6237badf660c4c754d2b513264996121c2b666d65d/mkl-2026.0.0-py2.py3-none-manylinux_2_28_x86_64.whl";
-    hash = "sha256-SpUl3dZxtCL+38aQ3ZJwvrZcjL1HBEtS0qTj9hYsneY=";
+    url = "https://files.pythonhosted.org/packages/6d/b4/ef531295ed33b929c6c5214421eeebe370f1be22536b6956b4aaf18fdbc5/mkl-2025.3.0-py2.py3-none-manylinux_2_28_x86_64.whl";
+    hash = "sha256-e4H10uA0Y3sYfKWGZoeMToiJmIp4454LbukuhGVo9mA=";
   };
 
   dependencies = [
-    (callPackage ./onemkl-license.nix { }) # ==2026.0.0
-    (callPackage ./intel-openmp.nix { }) # <2027,>=2025
-    (callPackage ./tbb.nix { }) # ==2023.*
+    (callPackage ./onemkl-license.nix { }) # ==2025.3.0
+    (callPackage ./intel-openmp.nix { }) # <2026,>=2024
+    (callPackage ./tbb.nix { }) # ==2022.*
   ];
 
   # Add dependency libraries to runtime path of mkl libs. Do this
   # postFixup as patchelf doesn't detect undeclared dependencies.
   postFixup = ''
-    find "$out" \( -iname '*.so' -o -iname '*.so.*' \) -exec patchelf \
+    find $out \( -name '*.so' -o -name '*.so.*' \) -exec patchelf \
       --add-rpath ${lib.makeLibraryPath dependencies} {} \;
   '';
 
